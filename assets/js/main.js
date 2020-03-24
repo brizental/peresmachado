55ec2767646bca09d2f032d48c716b095fa3a297
Beatriz Rizental <beatriz.rizental@gmail.com>
2020-03-24 11:22:15 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// Start slider.
+	$('.home-slider')
+	.slick({
+		arrows: false,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		speed: 1000
+	});
+});
